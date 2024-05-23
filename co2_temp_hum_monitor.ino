138322eeff1c35aa38638da0fe115f6adc8ab622
Superior404 <93605138+Superior404@users.noreply.github.com>
2024-05-23 22:24:54 +0200

    added c file

---
+++ co2_temp_hum_monitor.ino
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2021, Sensirion AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Sensirion AG nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <Arduino.h>
+#include <SensirionI2CScd4x.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+
+
+
+SensirionI2CScd4x scd4x;
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+void printUint16Hex(uint16_t value) {
+    Serial.print(value < 4096 ? "0" : "");
+    Serial.print(value < 256 ? "0" : "");
+    Serial.print(value < 16 ? "0" : "");
+    Serial.print(value, HEX);
+}
+
+void printSerialNumber(uint16_t serial0, uint16_t serial1, uint16_t serial2) {
+    Serial.print("Serial: 0x");
+    printUint16Hex(serial0);
+    printUint16Hex(serial1);
+    printUint16Hex(serial2);
+    Serial.println();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+  while (!Serial) {
+      delay(100);
+  }
+
+  Wire.begin();
+
+  uint16_t error;
+  char errorMessage[256];
+
+  scd4x.begin(Wire);
+
+  // stop potentially previously started measurement
+  error = scd4x.stopPeriodicMeasurement();
+  if (error) {
+      Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+  }
+
+  uint16_t serial0;
+  uint16_t serial1;
+  uint16_t serial2;
+  error = scd4x.getSerialNumber(serial0, serial1, serial2);
+  if (error) {
+      Serial.print("Error trying to execute getSerialNumber(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+  } else {
+      printSerialNumber(serial0, serial1, serial2);
+  }
+
+  // Start Measurement
+  error = scd4x.startPeriodicMeasurement();
+  if (error) {
+      Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+  }
+
+  Serial.println("Waiting for first measurement... (5 sec)");
+}
+
+void loop() {
+  uint16_t error;
+  char errorMessage[256];
+
+  display.clearDisplay(); // Clear display buffer
+  delay(5000);
+  // Read Measurement
+  uint16_t co2;
+  float temperature;
+  float humidity;
+  error = scd4x.readMeasurement(co2, temperature, humidity);
+  if (error) {
+      Serial.print("Error trying to execute readMeasurement(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+  } else if (co2 == 0) {
+      Serial.println("Invalid sample detected, skipping.");
+  } else {
+      display.clearDisplay();
+
+      display.setTextColor(WHITE);
+      display.setTextSize(2);
+      display.setCursor(0,0);
+      display.print("Room-Moni:");
+
+      display.setCursor(0,32);
+      display.setTextSize(1);
+      display.print("CO2:   ");
+      display.println(co2);
+      display.print("Temp:  ");
+      display.println(temperature);
+      display.print("Hum:   ");
+      display.println(humidity);
+      display.display();
+
+      if (co2 > 999) {
+        display.clearDisplay();
+        display.setCursor(0,0);
+        display.setTextSize(2);
+        display.print("Lueften!");
+
+        display.setCursor(0,32);
+        display.print("CO2: ");
+        display.println(co2);
+        display.display();
+        delay(2000);
+      }
+      /*
+      Serial.print("Co2:");
+      Serial.print(co2);
+      Serial.print("\t");
+      Serial.print("Temperature:");
+      Serial.print(temperature);
+      Serial.print("\t");
+      Serial.print("Humidity:");
+      Serial.println(humidity);
+      */
+  }
+
+}
